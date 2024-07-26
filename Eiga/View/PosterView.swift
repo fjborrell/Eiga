27f7fc180b7121b12a88e98e5d951f07c34e07e6
fjborrell <83686662+fjborrell@users.noreply.github.com>
2024-07-25 22:17:56 -0500

    +Modularized PosterView implementation

--- Eiga/View/PosterView.swift
+++ Eiga/View/PosterView.swift
@@ -10,71 +10,127 @@ import SwiftUI
 
 struct PosterView<M: Media>: View {
     let media: M
-    let size: TMBDImageConfig.PosterSize = .original
-
+    @State var height: CGFloat = 176
     @State private var posterURL: URL?
-    @State private var errorMessage: String?
+    @State var imageQuality: TMBDImageConfig.PosterSize = .w500
+    @State var posterError: Error?
     
     var body: some View {
-        VStack() {
-            AsyncImage(url: posterURL) { state in
-                switch state {
-                case .empty:
-                    ProgressView()
-                case .success(let image):
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .cornerRadius(10)
-                case .failure:
-                    Image(systemName: "photo")
+        VStack(spacing: 9) {
+            self.posterImage
+            self.posterLabel
+        }
+    }
+    
+    var posterImage: some View {
+        AsyncImage(url: self.posterURL) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+                    .aspectRatio(contentMode: .fit)
+            case .success(let image):
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+            case .failure:
+                MissingImageView(error: posterError)
+            @unknown default:
+                EmptyView()
+            }
+        }
+        .frame(height: self.height)
+        .onAppear {
+            loadPosterURL()
+        }
+    }
+    
+    var posterLabel: some View {
+        HStack(alignment: .top) {
+            Circle()
+                .frame(width: 17, height: 17)
+                .opacity(0.25)
+                .overlay(
+                    Image(systemName: MediaMode.movie.iconName)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .foregroundColor(.gray)
-                @unknown default:
-                    EmptyView()
-                }
-            }
-            .frame(height: 176)
-            .onAppear {
-                loadPosterURL()
-            }
+                        .frame(height: 10)
+                )
+                .foregroundStyle(MediaMode.movie.color)
             
-            if let errorMessage = errorMessage {
-                Text(errorMessage)
-                    .font(.caption)
-                    .foregroundColor(.red)
-                    .multilineTextAlignment(.center)
-            }
-            
-            HStack(alignment: .top) {
-                ZStack {
-                    Circle()
-                        .fill(MediaMode.movie.color.opacity(0.35))
-                        .frame(width: 14, height: 14)
-                    Image(systemName: MediaMode.movie.iconName)
-                        .imageScale(.small)
-                        .foregroundStyle(MediaMode.movie.color)
-                }
-                
-                Text(media.title)
-                    .font(.manrope(12))
-                    .lineLimit(2, reservesSpace: true)
-            }
-            .frame(width: <#T##CGFloat?#>)
-            
+            Text(media.title)
+                .font(.manrope(12))
+                .foregroundStyle(.white)
+                .lineLimit(2, reservesSpace: true)
         }
     }
     
     private func loadPosterURL() {
         do {
-            posterURL = try media.getPosterURL(size: size)
-        } catch ImageError.missingImagePath {
-            errorMessage = "No poster available"
-        } catch ImageError.invalidURL {
-            errorMessage = "Invalid poster URL"
+            posterURL = try media.getPosterURL(size: self.imageQuality)
+            posterError = nil
         } catch {
-            errorMessage = "Unexpected error"
+            posterError = error
+            posterURL = nil
         }
     }
+}
+
+struct MissingImageView: View {
+    var error: Error?
+    var body: some View {
+        VStack(spacing: 5) {
+            Image(systemName: "photo.badge.exclamationmark")
+                .imageScale(.large)
+                .symbolRenderingMode(.multicolor)
+                .foregroundStyle(.white, .red)
+            Text(error?.localizedDescription ?? "Error")
+                .font(.caption)
+                .foregroundColor(.red)
+                .lineLimit(5)
+                .multilineTextAlignment(.center)
+        }
+    }
+    
+    private var errorMessage: String {
+        if let errorMessage = error as? ImageError {
+            switch errorMessage {
+            case .missingImagePath:
+                return "No Poster Available"
+            case .invalidURL:
+                return "Invalid Image URL"
+            }
+        }
+        return error?.localizedDescription ?? "Unknown Error"
+    }
+}
+
+// MARK: - Preview
+
+private struct PreviewWrapper: View {
+    let tmbd = TMBDService()
+    @State private var movie: Movie?
+    
+    var body: some View {
+        Group {
+            if let movie = movie {
+                PosterView(media: movie)
+            } else {
+                ProgressView()
+            }
+        }
+        .frame(width: 150, height: 200)
+        .task {
+            do {
+                self.movie = try await tmbd.fetchMovie(id: 810693)
+            } catch {
+                print("Error fetching movie: \(error)")
+            }
+        }
+    }
+}
+
+#Preview {
+    PreviewWrapper()
+        .hueBackground(hueColor: .pink)
 }
